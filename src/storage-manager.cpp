561e3fceb38daa4689660cbd42f939b3ef72620a
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2023-12-17 18:08:48 +0100

    separated the b tree storage handler from BTree class

---
+++ src/storage-manager.cpp
@@ -0,0 +1,58 @@
+
+#include<fstream>
+#include<iostream>
+
+template <typename T>
+
+class BTreeStorageHandler{
+    private:
+	BTreeNode<T>* root;
+	std::fstream file;
+    public:
+	StorageHandler(BTreeNode<T>* rootNode, const std::string& filename="default_btree.dat") : root(rootNode){
+	    if (!root){
+		throw std::invalid_argument("Error: root cannot be nullptr");
+	    }
+	    file.open(filename, std::ios::in | std::ios::out | std::ios::binary | std::ios::trunc);
+
+	    if (!file.is_open()){
+		throw std::runtime_error("Error: unable to open the file");
+	    }
+	}
+
+	void saveToFile(){
+	    file.seekp(0);
+	    writeToFile(root);
+	}
+
+	void loadFromFile(){
+	    file.seekg(0);
+	    root=readFromFile();
+	}
+
+	~BTreeStorageHandler(){
+	    file.close();
+	}
+
+
+    private:
+	void writeToFile(BTreeNode<T>* node){
+	    if (node!=nullptr){
+		file.write(reinterpret_cast<char*>(node), sizeof(BTreeNode<T>));
+
+		for (size_t i=0; i<node->children.size(); ++i){
+		    writeToFile(node->children[i]);
+		}
+	    }
+	}
+
+	BTreeNode<T>* readFromFile(){
+	    BTreeNode<T>* node = new BTreeNode<T>();
+	    file.read(reinterpret_cast<char*>(node), sizeof(BTreeNode<T>));
+	    for (size_t i=0;i<node->children.size();++i){
+		    node->children[i]=readFromFile();
+	    }
+
+	    return node;
+	}
+};
--- src/structs.cpp
+++ src/structs.cpp
@@ -18,9 +18,7 @@ class BTree{
 	std::fstream file;
 
     public:
-	BTree(const std::string& filename="default_btree.dat") : root(nullptr) {
-	    file.open(filename, std::ios::in | std::ios::out | std::ios:binary | std::ios::trunc);
-	}
+	BTree() : root(nullptr) {}
 
 	void insert(const T& key){
 	    if (root==nullptr){
@@ -44,20 +42,6 @@ class BTree{
 	void print() const{
 	    inOrderTraversal(root);
 	    std::cout << std::endl;
-	}
-
-	void saveToFile(){
-	    file.seekp(0);
-	    writeToFile(root);
-	}
-
-	void loadFromFile(){
-	    file.seekg(0);
-	    root = readFromFile();
-	}
-	
-	~BTree(){
-	    file.close();
 	}
 
     private:
@@ -139,25 +123,5 @@ class BTree{
 	    parentNode->children.insert(parentNode->children.begin() + childIndex + 1, newChild);
 	}
 
-	void writeToFile(BTreeNode<T>* node){
-	    if (node!=nullptr){
-		file.write(reinterpret_cast<char*>(node), sizeof(BTreeNode<T>));
-
-		for (size_t i=0; i<node->children.size(); ++i){
-		    writeToFile(node->children[i]);
-		}
-	    }
-	}
-
-	BTreeNode<T>* readFromFile(){
-	    BTreeNode<T>* node = new BTreeNode<T>();
-	    file.read(reinterpret_cast<char*>(node), sizeof(BTreeNode<T>));
-
-	    for (size_t i=0; i<node->children.size(); ++i){
-		node->children[i]=readFromFile();
-	    }
-
-	    return node;
-	}
 };
 
