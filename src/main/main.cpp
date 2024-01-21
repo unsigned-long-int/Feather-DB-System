34910bdebf3b589d17a7f36ab226c4bb28a4c284
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2024-01-21 18:00:44 +0100

    add basic command handling

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include <string>
-#include <unordered_map>
 
-int main(){
-    return 0;
+int main(int argc, char* argv[]){
+
+    bool applicationRunning = true;
+    while (applicationRunning){
+	std::cout << "db > \n";
+
+	std::string sqlCommand;
+
+	if(!(std::cin >> sqlCommand)){
+	    std::cout << "Error reading input. \n";
+	    return EXIT_FAILURE;
+	}
+
+	switch(sqlCommand){
+	    case ".exit": {
+			      std::cout << "The request has been terminated.\n";
+			      return EXIT_SUCCESS
+
+			  }
+	    default: {
+			 std::cout << "unrecognised command " sqlCommand ". \n";
+		     }
+	}
+    }
+    return EXIT_SUCCESS;
 }
