83e43ab6df2b4b7bf2d67605667e892112e5698a
OleynikovNikolay <nikolay.oleynikov96@gmail.com>
2024-01-27 20:21:44 +0100

    changed main function

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]){
 	    continue;
 	}
 
-	if (sql_parser)
+	std::unique_ptr<CommandsHandler> sql_command_instance = sql_parser.fetchSQLHandlerInstance();
     }
     return EXIT_SUCCESS;
 }
